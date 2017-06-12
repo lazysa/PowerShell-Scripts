83e254fb567e52f36bcc4f3e1ac9301c41adfeba
xu.chen <xu.chen@@chiefclouds.com>
2017-06-12 18:45:32 +0800

    'Change by hex system'

--- sqltable-to-dat.ps1
+++ sqltable-to-dat.ps1
@@ -4,7 +4,7 @@
  Powershell script
 #>
 
-$arg = '-t, -r\n -c -T -k -C65001'
+$arg = '-t, -r0x0A -c -T -k -C65001'
 $dir = 'D:\table'
 $log = 'D:\log'
 $db0 = 'buy2016'
@@ -15,7 +15,7 @@ $num0 = $db0_tb.Length
 for ($i=0; $i -lt "$num0"; $i++)
 {
     $tb0 = $db0_tb[$i]
-    bcp "SELECT FLOWNO,POSNO,ITEMNO,GID,QTY,PRICE,REALAMT,LSTUPDTIME,fildate,gdcode,dealtime FROM $db0.dbo.$tb0" queryout "$dir\$tb0.dat" -o "$log\$tb0.log" "-t," -r\n -c -T -k -C65001 
+    bcp "SELECT FLOWNO,POSNO,ITEMNO,GID,QTY,PRICE,REALAMT,LSTUPDTIME,fildate,gdcode,dealtime FROM $db0.dbo.$tb0" queryout "$dir\$tb0.dat" -o "$log\$tb0.log" "-t," -r0x0A -c -T -k -C65001 
 }
 
 
@@ -26,7 +26,7 @@ $num0 = $db1_tb.Length
 for ($i=0; $i -lt "$num0"; $i++)
 {
     $tb1 = $db1_tb[$i]
-    bcp "SELECT FLOWNO,POSNO,ITEMNO,GID,QTY,PRICE,REALAMT,LSTUPDTIME,fildate,gdcode,dealtime FROM $db1.dbo.$tb1" queryout "$dir\$tb1.dat" -o "$log\$tb1.log" "-t," -r\n -c -T -k -C65001  
+    bcp "SELECT FLOWNO,POSNO,ITEMNO,GID,QTY,PRICE,REALAMT,LSTUPDTIME,fildate,gdcode,dealtime FROM $db1.dbo.$tb1" queryout "$dir\$tb1.dat" -o "$log\$tb1.log" "-t," -r0x0A -c -T -k -C65001   
 }
 
 
@@ -40,14 +40,14 @@ $num0 = $db2_tb0.Length
 for ($i=0; $i -lt "$num0"; $i++)
 {
     $tb0 = $db2_tb0[$i]
-    bcp "SELECT FLOWNO,POSNO,ITEMNO,GID,QTY,PRICE,REALAMT,'',fildate,gdcode,dealtime FROM $db2.dbo.$tb0" queryout "$dir\$tb0.dat" -o "$log\$tb0.log" "-t," -r\n -c -T -k -C65001
+    bcp "SELECT FLOWNO,POSNO,ITEMNO,GID,QTY,PRICE,REALAMT,'',fildate,gdcode,dealtime FROM $db2.dbo.$tb0" queryout "$dir\$tb0.dat" -o "$log\$tb0.log" "-t," -r0x0A -c -T -k -C65001 
 }
 
 $num1 = $db2_tb1.Length
 for ($i=0; $i -lt "$num1"; $i++)
 {
     $tb1 = $db2_tb1[$i]
-    bcp "SELECT FLOWNO,POSNO,ITEMNO,GID,QTY,PRICE,REALAMT,LSTUPDTIME,fildate,gdcode,dealtime FROM $db2.dbo.$tb1" queryout "$dir\$tb1.dat" -o "$log\$tb1.log" "-t," -r\n -c -T -k -C65001 
+    bcp "SELECT FLOWNO,POSNO,ITEMNO,GID,QTY,PRICE,REALAMT,LSTUPDTIME,fildate,gdcode,dealtime FROM $db2.dbo.$tb1" queryout "$dir\$tb1.dat" -o "$log\$tb1.log" "-t," -r0x0A -c -T -k -C65001 
 }
 
 
@@ -60,14 +60,14 @@ $num0 = $db3_tb0.Length
 for ($i=0; $i -lt "$num0"; $i++)
 {
     $tb0 = $db3_tb0[$i]
-    bcp "SELECT FLOWNO,POSNO,ITEMNO,GID,QTY,PRICE,REALAMT,LSTUPDTIME,fildate,gdcode,'' FROM $db3.dbo.$tb0" queryout "$dir\$tb0.dat" -o "$log\$tb0.log" "-t," -r\n -c -T -k -C65001 
+    bcp "SELECT FLOWNO,POSNO,ITEMNO,GID,QTY,PRICE,REALAMT,LSTUPDTIME,fildate,gdcode,'' FROM $db3.dbo.$tb0" queryout "$dir\$tb0.dat" -o "$log\$tb0.log" "-t," -r0x0A -c -T -k -C65001  
 }
 
 $num1 = $db3_tb1.Length
 for ($i=0; $i -lt "$num1"; $i++)
 {
     $tb1 = $db3_tb1[$i]
-    bcp "SELECT FLOWNO,POSNO,ITEMNO,GID,QTY,PRICE,REALAMT,LSTUPDTIME,fildate,gdcode,dealtime FROM $db3.dbo.$tb1" queryout "$dir\$tb1.dat" -o "$log\$tb1.log" "-t," -r\n -c -T -k -C65001
+    bcp "SELECT FLOWNO,POSNO,ITEMNO,GID,QTY,PRICE,REALAMT,LSTUPDTIME,fildate,gdcode,dealtime FROM $db3.dbo.$tb1" queryout "$dir\$tb1.dat" -o "$log\$tb1.log" "-t," -r0x0A -c -T -k -C65001 
 }
 
 
